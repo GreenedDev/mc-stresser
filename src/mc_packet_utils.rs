87752895ba041af2e7c9d6d456d279544d6c9648
Greened <greeneddev@gmail.com>
2025-08-16 14:27:43 +0400

    feat: rewrite mc proto & dont depend on random ass crate

--- src/mc_packet_utils.rs
+++ src/mc_packet_utils.rs
@@ -1,18 +1,85 @@
-use rust_mc_proto_tokio::{DataWriter, MCConnTcp, Packet};
+use tokio::{io::AsyncWriteExt, net::TcpStream};
 
 // Send handshake packet to initiate connection
 pub async fn send_handshake(
-    conn: &mut MCConnTcp,
+    stream: &mut TcpStream,
     proto: u16,
     hostname: &str,
     srv_port: u16,
     next_state: u8,
 ) {
-    let mut packet = Packet::empty(0x00);
+    _ = write_u8_varint(stream, 0x00).await;
 
-    _ = packet.write_u16_varint(proto).await;
-    _ = packet.write_string(hostname).await;
-    _ = packet.write_unsigned_short(srv_port).await;
-    _ = packet.write_u8_varint(next_state).await;
-    _ = conn.write_packet(&packet).await;
+    _ = write_u16_varint(stream, proto).await;
+    _ = write_string(stream, hostname).await;
+    _ = write_unsigned_short(stream, srv_port).await;
+    _ = write_u8_varint(stream, next_state).await;
+    _ = stream.flush().await;
+}
+pub async fn write_u8_varint(stream: &mut TcpStream, val: u8) {
+    let mut value = val;
+    loop {
+        if (value & !0x7F) == 0 {
+            if stream.write_all(&[value]).await.is_err() {
+                break;
+            }
+            return;
+        }
+
+        if stream.write_all(&[((value & 0x7F) | 0x80)]).await.is_err() {
+            break;
+        }
+
+        value >>= 7;
+    }
+}
+pub async fn write_u16_varint(stream: &mut TcpStream, val: u16) {
+    let mut value = val;
+    loop {
+        if (value & !0x7F) == 0 {
+            if stream.write_all(&[value as u8]).await.is_err() {
+                break;
+            }
+            return;
+        }
+
+        if stream
+            .write_all(&[((value & 0x7F) | 0x80) as u8])
+            .await
+            .is_err()
+        {
+            break;
+        }
+
+        value >>= 7;
+    }
+}
+pub async fn write_usize_varint(stream: &mut TcpStream, val: usize) {
+    let mut value = val;
+    loop {
+        if (value & !0x7F) == 0 {
+            if stream.write_all(&[value as u8]).await.is_err() {
+                break;
+            }
+            return;
+        }
+
+        if stream
+            .write_all(&[((value & 0x7F) | 0x80) as u8])
+            .await
+            .is_err()
+        {
+            break;
+        }
+
+        value >>= 7;
+    }
+}
+pub async fn write_string(stream: &mut TcpStream, val: &str) {
+    let bytes = val.as_bytes();
+    write_usize_varint(stream, bytes.len()).await;
+    let _ = stream.write_all(bytes).await;
+}
+pub async fn write_unsigned_short(stream: &mut TcpStream, val: u16) {
+    let _ = stream.write_all(&val.to_be_bytes()).await;
 }
--- src/method_join.rs
+++ src/method_join.rs
@@ -1,24 +1,24 @@
-use rust_mc_proto_tokio::{DataWriter, MCConnTcp, Packet};
+use tokio::{io::AsyncWriteExt, net::TcpStream};
 use uuid::Uuid;
 
-use crate::mc_packet_utils::send_handshake;
+use crate::mc_packet_utils::{send_handshake, write_string, write_u8_varint};
 
-pub async fn send_join(conn: &mut MCConnTcp, port: &u16, hostname: &str) {
+pub async fn send_join(stream: &mut TcpStream, port: &u16, hostname: &str) {
     let protocol_version = 770;
 
     // Switch to login state (2)
-    send_handshake(conn, protocol_version, hostname, *port, 2).await;
+    send_handshake(stream, protocol_version, hostname, *port, 2).await;
 
     // Send login start packet
-    send_login_start(conn, "test").await;
+    send_login_start(stream, "test").await;
 
     //_ = conn.write_packet(&Packet::empty(0x03)).await;
 }
 
 // Send login start packet
-pub async fn send_login_start(conn: &mut MCConnTcp, username: &str) {
-    let mut packet = Packet::empty(0x00);
-    _ = packet.write_string(username).await;
-    _ = packet.write_uuid(&Uuid::default()).await; // No UUID for offline mode
-    _ = conn.write_packet(&packet).await;
+pub async fn send_login_start(stream: &mut TcpStream, username: &str) {
+    _ = write_u8_varint(stream, 0x00).await;
+
+    _ = write_string(stream, username).await;
+    _ = stream.write_all(Uuid::default().as_bytes()).await; // No UUID for offline mode
 }
